e633e91bbdbdc31b5b93c9518c2f6badcf035565
shekeine <shekeinespere@gmail.com>
2016-02-25 16:10:58 +0100

    check scoping issue

--- R/stratPlot_varlist.R
+++ R/stratPlot_varlist.R
@@ -4,19 +4,19 @@
 #dat_grp=dat$Herb
 
 stratPlot_varlist <- function(dat_grpL){
-  
+
                               #Apply stratplot_i over variables in i'th group
-                              source("fun/stratPlot_var.R", local=T)
+                              #source("fun/stratPlot_var.R", local=T)
                               gglist <- lapply(X=dat_grpL, FUN=stratPlot_var)
-                              
+
                               #Order gglist plots by group and maxv
                               gglist <- gglist[as.character(dat_ctrl[, variable])]
-                              
+
                               #Stitch result into one plot with all variables (taxa) in i'th group
 
                                 #Make stitch args
                                 args.list <- list(ncol=length(gglist), widths=lapply(c(dat_ctrl[, pwidth]), unit, "cm"))
-                                
+
                                 #Stitch
                                 grob_grp   <- do.call(arrangeGrob, c(gglist, args.list))
 
@@ -25,10 +25,10 @@ stratPlot_varlist <- function(dat_grpL){
                                     grob_grp[["grobs"]][[nm]]$heights[grob_grp[["grobs"]][[nm]]$layout$name=="strip_t-1"] <- unit(strip_height, 'cm')
                                     grob_grp[["grobs"]][[nm]]$grobs[["strip_t"]]$heights <- unit(1, 'native')
                                   }
-                                  
+
                                   #Turn title clipping off to allow full display of facet titles even when rotated
                                   for(nm in names(gglist)){grob_grp[["grobs"]][[nm]]$grobs$strip_t$layout$clip <- "off"}
-                                  
+
                                   #Reverse z index to allow left-right overlap
                                   grob_grp$layout$z <- rev(grob_grp$layout$z)
 
@@ -39,7 +39,7 @@ stratPlot_varlist <- function(dat_grpL){
                                 #Get height of strip
                                 glab_ht <- unit((as.numeric(tstrip$grobs[[1]]$heights)), 'cm')
                                 grob_grp <- arrangeGrob(grob_grp, top=tstrip, clip=F, padding=glab_ht)
-                                
+
 #                               grid.newpage()
 #                               grid.draw(grob_grp)
 
@@ -47,9 +47,9 @@ stratPlot_varlist <- function(dat_grpL){
                               #margin width assumes that each char of the final plot's label takes up 0.2cm
                               mw <- nchar(as.character(dat_ctrl[nrow(dat_ctrl), variable])) * 0.25
                               grob_grp <- gtable_add_cols(grob_grp, unit(mw,"cm"))
-                                    
+
 #                               grid.newpage()
 #                               grid.draw(grob_grp)
-                              
+
                             return(grob_grp)
                           }
